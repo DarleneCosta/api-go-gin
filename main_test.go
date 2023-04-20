dded8c46d064acb5ab944777b689a5b92e3e36b2
Darlene Costa <darl_mel@hotmail.com>
2023-04-20 05:16:02 -0300

    init test in go

---
+++ main_test.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"github/DarleneCosta/api-go-gin/controllers"
+	"github/DarleneCosta/api-go-gin/database"
+	"github/DarleneCosta/api-go-gin/models"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func SetupDasRotasDeTeste() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
+	rotas := gin.Default()
+	return rotas
+}
+
+var ID int
+
+func CriaAlunoMock() {
+	aluno := models.Aluno{
+		Nome: "Nome do Aluno Teste",
+		CPF:  "12345678901",
+		RG:   "123456789",
+	}
+	database.DB.Create(&aluno)
+	ID = int(aluno.ID)
+}
+func DeletaAlunoMock() {
+	var aluno models.Aluno
+	database.DB.Delete(&aluno, ID)
+}
+
+func TestVerificaStatusCodeDaSaudacao(t *testing.T) {
+	r := SetupDasRotasDeTeste()
+	r.GET("/:nome", controllers.Saudacao)
+	req, _ := http.NewRequest("GET", "/teste", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code, "Deveriam ser iguais")
+	mockDaResposta := `{"API diz":"E ai teste, tudo beleza?"}`
+	respostaBody, _ := ioutil.ReadAll(resposta.Body)
+	assert.Equal(t, mockDaResposta, string(respostaBody))
+}
+
+func TestListandoTodosOsAlunosHandler(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos", controllers.ExibeTodosAlunos)
+	req, _ := http.NewRequest("GET", "/alunos", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
+}
+
+func TestBuscaAlunoPorCpfHandler(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
+	req, _ := http.NewRequest("GET", "/alunos/cpf/12345678901", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
+}
